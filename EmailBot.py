c71e48c3b7dc90d5326232693ac25a07f0ab2232
SIDDMATRIX <167300236+SIDDMATRIX@users.noreply.github.com>
2024-04-17 15:48:43 +0530

    Create EmailBot.py

---
+++ EmailBot.py
@@ -0,0 +1,54 @@
+import pandas as p
+import smtplib as sm
+
+
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+data = p.read_excel("students.xlsx")
+# print(type(data))
+# print(data.str())
+email_col = data.get("email")
+list_of_emails = list(email_col)
+print(list_of_emails)
+
+try:
+    server = sm.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login("email@gmail.com", 'passward')
+    from_ = "avinashverma9504@gmail.com"
+    to_ = list_of_emails
+    message = MIMEMultipart("alternative")
+    message['subject'] = "This is just testing message"
+    message["from"] = "avinashverma9504@gmail.com"
+
+    html = '''
+    <html>
+    <head>
+
+    </head>
+    <body>
+        <h1>This is shreyansh verma</h1>
+        <h2>i just want to say hii</h2>
+        <p>this id for testing</p>
+        <button style="padding:20px;background:green;color:white">varify</button>
+
+    </body>
+
+    </html>
+
+
+    '''
+
+    part2 = MIMEText(html, "html")
+
+    message.attach(part2)
+    server.sendmail(from_, to_, message.as_string())
+    print("message has been to the emails")
+
+
+
+except Exception as e:
+      print(e)
